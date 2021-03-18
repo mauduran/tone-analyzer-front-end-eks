3d937a5049f441f3b6ac02ad773ca7aea6dcd2cc
mauduran <is716122@iteso.mx>
2021-03-18 16:41:00 -0600

    Added index.html view structure and functionality

---
+++ public/scripts/main.js
@@ -0,0 +1,46 @@
+
+var button = document.getElementById("analizer-btn");
+var clearBtn = document.getElementById("clear-btn");
+console.log(clearButton);
+var text = document.getElementById("text-to-analyze");
+var resultDiv = document.getElementById("result");
+
+const URL = 'http://tone-analyzer-app-mauduran.us-south.cf.appdomain.cloud/tone'
+
+function getToneOfText() {
+    content = text.value.trim();
+    if (content == "") return
+    resultDiv.innerHTML = `    
+    <div class="d-flex justify-content-center">
+        <div class="spinner-border text-primary" style="width: 4rem; height:4rem" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>`;
+
+    fetch(URL, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ content })
+    }).then((res) => res.json()
+    ).then((result) => {
+        console.log(result);
+        resultDiv.innerHTML = '<p class="h1" style="text-align:center">-Results- Your text\'s tone is:</p>';
+
+        result.tones.forEach((tone)=>{
+            resultDiv.innerHTML += `<p style="text-align:center" class="display-6">${tone.tone_name} - ${(tone.score*100).toFixed(2)}%<p>`
+        })
+    }).catch((err) => {
+        resultDiv.innerHTML = 'Could not get tone analysis.'
+        resultDiv.style = "color:red;"
+    });
+
+}
+
+function clearButton() {
+    text.value = "";
+}
+button.addEventListener("click", getToneOfText);
+clearBtn.addEventListener("click", clearButton);
